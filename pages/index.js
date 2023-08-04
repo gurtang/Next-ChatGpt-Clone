88d6fc839218b621d2cf93d244426cc363bf6eac
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-08-04 15:07:58 +0200

    Implement Signup functionality

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
                 Login
               </Link>
               <Link
-                href={"/api/auth/singup"}
+                href={"/api/auth/signup"}
                 className={
                   "rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600 "
                 }
