89480a5bfea34e969568cce5fae351bb6d4e3943
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-08-04 14:59:13 +0200

    Create Login and signup buttons

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
 import "../styles/globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-      </Head>
-      <Component {...pageProps} />
+      <UserProvider>
+        <Head>
+          <title>Chatty Pete</title>
+          <link rel="icon" href="/favicon.png" />
+        </Head>
+        <Component {...pageProps} />
+      </UserProvider>
     </>
   );
 }
